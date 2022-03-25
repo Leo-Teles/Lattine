cf172e09538ac6be32a96b225376af02533b567c
Leonardo Teles De Souza Menezes <46881551827@info.scs>
2022-03-25 11:18:24 -0300

    Login quase finalizado

--- Front-End/lattine-ui/src/Pages/Login/Login.js
+++ Front-End/lattine-ui/src/Pages/Login/Login.js
@@ -2,25 +2,48 @@ import { Component } from "react";
 
 import './Login.css'
 
+import imgEsquerda from '../Assets/imgLogin.png'
+import imgUsuario from '../Assets/pessoa.png'
+import imgCadeado from '../Assets/cadeado.png'
+
+
 
 
 export default class Login extends Component {
-    
 
-    render(){
-        return(
-            <div>
 
-                <div class="primeiroBloco">
+    render() {
+        return (
+            <div className="blocoTodo">
 
-                 <h1 class="bemVindo">Bem-Vindo</h1>
-                 <p class="semConta">Não possui uma conta?</p>
+                <div className="primeiroBloco">
 
-                 <button>Cadastre-se</button>
+                    <div className="teste-div">
+                        <h1 className="bemVindo">Bem-Vindo</h1>
+                        <p className="semConta">Não possui uma conta?</p>
 
-                
+                        <button className="cadastro">Cadastre-se</button>
+                    </div>
+
+                    <img className="imgLogin" src={imgEsquerda} />
+                </div>
+
+                <div className="segundoBloco">
+
+                    <img className="imgUsuario" src={imgUsuario}/>
+
+                    <input className="inputInicial"/> 
+
+                    <img className="imgCadeado" src={imgCadeado} />
+
+                    <input className="inputInicial"/>
+
+                    <button className="entrar"> Entrar </button>
+
+
 
                 </div>
+
 
 
             </div>
