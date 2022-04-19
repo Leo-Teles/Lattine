6985b6947f72dbea3552d278c644fd62e3fe47bc
Leonardo Teles De Souza Menezes <46881551827@info.scs>
2022-04-19 11:14:06 -0300

    home

--- Front-End/lattine-ui/src/pages/Home/Home.jsx
+++ Front-End/lattine-ui/src/pages/Home/Home.jsx
@@ -4,23 +4,49 @@ import { Link } from "react-router-dom";
 import '../../assets/css/Home.css';
 
 import LogoLattine from '../../assets/img/LogoLattine.png'
+import imgHome from '../../assets/img/imgHome.png'
 
 export default class Home extends Component {
-    render(){
-        return(
+
+
+
+    render() {
+        return (
             <div className="ConteudoTodo">
-                <header className="headerHome"> 
-                <img className="imgLattine"  src={LogoLattine}/>
-                <Link className="BotaoLogin" to="/login"> Login </Link>
+                <header className="headerHome">
+                    <img className="imgLattine" src={LogoLattine} />
+                    <Link className="BotaoLogin" to="/login"> Login </Link>
                 </header>
 
                 <div className="Conteudo">
 
                     <div className="primeiroConteudo">
 
-                        <h1 className="FraseImpacto"> Pense diferente e experimente o poder de conectar pessoas!</h1>
+                        <div className="seguraConteudo">
+                            <h1 className="FraseImpacto"> Pense diferente e experimente o poder de conectar pessoas!</h1>
+                            <p className="textoBaixo"> Transformamos a maneira como empresas trabalham e produzem a partir da tecnologia e qualidade. Soluções inovadoras para organizações que buscam resultados extraordinários.</p>
+                            <Link className="cadastre" to="/cadastro">Cadastre-se</Link>
+                        </div>
 
-                        <p className="textoBaixo"> Transformamos a maneira como empresas trabalham e produzem a partir da tecnologia e qualidade. Soluções inovadoras para organizações que buscam resultados extraordinários.</p>
+                        <img className="imgHome" src={imgHome} />
+
+                    </div>
+
+                    <div className="seguendoConteudo">
+
+                        <h2 className="servicosOfertados">Nossos serviços </h2>
+
+                        <from>
+                            <button > Máquinas virtuais </button>
+
+                            <button> Redes virtuais</button>
+
+                            <button> Serviços aplicacionais</button>
+                        </from>
+
+
+
+
 
                     </div>
 
